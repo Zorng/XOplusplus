b1235a8f61466afba723c900ee7be1b35807b862
Tong Vorak <vorak.tong@student.cadt.edu.kh>
2025-02-28 20:47:30 +0700

    complete rapid mode

--- src/application/controllers/GameOptionSceneController.java
+++ src/application/controllers/GameOptionSceneController.java
@@ -28,11 +28,18 @@ public class GameOptionSceneController {
     @FXML
     public void startButtonCondition(ActionEvent event) throws IOException {
         if (
-                (radiobutton1.isSelected() || radiobutton2.isSelected() || radiobutton3.isSelected()) && // One mode must be selected
-                        (radiobutton4.isSelected() || radiobutton5.isSelected()) && // One win condition must be selected
-                        (!radiobutton6.isSelected() || radiobutton6.isSelected()) // Rapid is optional (either selected or not)
+                (radiobutton1.isSelected() || radiobutton2.isSelected() || radiobutton3.isSelected()) &&  // One mode must be selected
+                        (radiobutton4.isSelected() || radiobutton5.isSelected()) &&  // One win condition must be selected
+                        (!radiobutton6.isSelected() || radiobutton6.isSelected())  // Rapid is optional (either selected or not)
         ) {
-            Route.get("classic3x3", event);
+            // Check if Rapid mode is selected
+            if (radiobutton6.isSelected()) {
+                // If Rapid mode is selected, route to rapid3x3 scene
+                Route.get("rapid3x3", event);
+            } else {
+                // If Rapid mode is not selected, route to classic3x3 scene
+                Route.get("classic3x3", event);
+            }
         }
     }
 }
